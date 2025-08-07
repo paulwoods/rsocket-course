0b164f9a8d37bfdc34798c0132220e9549049169
mrpaulwoods <mr.paul.woods@gmail.com>
2025-08-07 10:42:40 -0500

    feat: add server-side delay for fire-and-forget requests

--- simple-rsocket/src/main/java/org/mrpaulwoods/rsocket/service/MathService.java
+++ simple-rsocket/src/main/java/org/mrpaulwoods/rsocket/service/MathService.java
@@ -8,6 +8,16 @@ public class MathService implements RSocket {
 
     @Override
     public Mono<Void> fireAndForget(Payload payload) {
+
+        // shows that the client exits quickly (because it's fire and forget)
+        // and the server waits 5 seconds before accepting the request.
+        try {
+            //noinspection BlockingMethodInNonBlockingContext
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
         System.out.println("Receiving: " + payload.getDataUtf8());
         return Mono.empty();
     }
