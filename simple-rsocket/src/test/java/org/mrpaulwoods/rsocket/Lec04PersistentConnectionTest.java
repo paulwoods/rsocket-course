8395d87ec42fcbdfb7d1307080fbd9c83fd7c892
mrpaulwoods <mr.paul.woods@gmail.com>
2025-08-23 21:01:52 -0500

    feat: add persistent connection test for request-stream interactions
    - Introduced `Lec04PersistentConnectionTest` to ensure reconnection and request-stream handling.
    - Utilized `RSocketClient` for managing persistent connections.

---
+++ simple-rsocket/src/test/java/org/mrpaulwoods/rsocket/Lec04PersistentConnectionTest.java
@@ -0,0 +1,71 @@
+package org.mrpaulwoods.rsocket;
+
+import io.rsocket.Payload;
+import io.rsocket.RSocket;
+import io.rsocket.core.RSocketClient;
+import io.rsocket.core.RSocketConnector;
+import io.rsocket.transport.netty.client.TcpClientTransport;
+import io.rsocket.util.DefaultPayload;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import java.time.Duration;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class Lec04PersistentConnectionTest {
+
+    private RSocketClient rSocketClient;
+
+    @BeforeAll
+    public void beforeAll() {
+//        this.rSocket = RSocketConnector
+//                .create()
+//                .connect(TcpClientTransport.create("localhost", 6565))
+//                .block();
+
+        // if the connection to the server is broken, the client will try to reconnect
+        Mono<RSocket> socketMono = RSocketConnector
+                .create()
+                .connect(TcpClientTransport.create("localhost", 6565))
+                .doOnNext(r -> System.out.println("going to connect"));
+
+        rSocketClient = RSocketClient.from(socketMono);
+    }
+
+    @Test
+    public void connectionTest() throws InterruptedException {
+
+        Flux<String> flux1 = this.rSocketClient
+                .requestStream(Mono.just(DefaultPayload.create("")))
+                .map(Payload::getDataUtf8)
+                .delayElements(Duration.ofMillis(300))
+                .take(10)
+                .doOnNext(System.out::println);
+
+        StepVerifier.create(flux1)
+                .expectNextCount(10)
+                .verifyComplete();
+
+        System.out.println("going to sleep");
+        Thread.sleep(5000);
+        System.out.println("woke up");
+
+        Flux<String> flux2 = this.rSocketClient
+                .requestStream(Mono.just(DefaultPayload.create("")))
+                .map(Payload::getDataUtf8)
+                .delayElements(Duration.ofMillis(300))
+                .take(10)
+                .doOnNext(System.out::println);
+
+        StepVerifier.create(flux2)
+                .expectNextCount(10)
+                .verifyComplete();
+
+    }
+
+
+}
