d8193a36ec48093c88ddbb0e79e57a45276b79e5
mrpaulwoods <mr.paul.woods@gmail.com>
2025-08-23 20:47:28 -0500

    feat: add fast producer service and backpressure test
    - Introduced `FastProducerService` for handling high-volume request-streams.
    - Updated `SocketAcceptorImpl` to use `FastProducerService`.
    - Added `Lec03BackpressureTest` to verify backpressure handling.

--- simple-rsocket/src/main/java/org/mrpaulwoods/rsocket/service/SocketAcceptorImpl.java
+++ simple-rsocket/src/main/java/org/mrpaulwoods/rsocket/service/SocketAcceptorImpl.java
@@ -11,7 +11,8 @@ public class SocketAcceptorImpl implements SocketAcceptor {
     public Mono<RSocket> accept(ConnectionSetupPayload connectionSetupPayload, RSocket rSocket) {
         System.out.println("SocketAcceptorImpl-accept method");
 //        return Mono.fromCallable(MathService::new);
-        return Mono.fromCallable(() -> new BatchJobService(rSocket));
+//        return Mono.fromCallable(() -> new BatchJobService(rSocket));
+        return Mono.fromCallable(FastProducerService::new);
     }
 
 }
